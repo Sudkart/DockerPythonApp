a5cfafa9f1426bcf91ebafc6390c0370a49d5c2d
Sudharshan Karthik <globusitsudharshan@gmail.com>
2023-08-11 05:14:32 +0530

    updaing the docker file and app file

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,24 @@
+# FROM python:3.8
+
+# WORKDIR /app
+
+# COPY requirements.txt .
+
+# RUN pip install -r requirements.txt
+
+# COPY . .
+
+# CMD ["python", "app.py"]
+
+# Dockerfile
 FROM python:3.8
 
 WORKDIR /app
 
-COPY requirements.txt .
+COPY requirements.txt requirements.txt
 
 RUN pip install -r requirements.txt
 
 COPY . .
 
-CMD ["python", "app.py"]
+CMD ["streamlit", "run", "app.py"]
--- app.py
+++ app.py
@@ -1,14 +1,41 @@
+# import pandas as pd
+
+# def analyze_data(file_path):
+#     data = pd.read_excel("topic_detect_final_test.xlsx")
+    
+#     # Perform analysis on the data (you can customize this part)
+#     summary = data.describe()
+    
+#     return summary
+
+# if __name__ == "__main__":
+#     data_file = input("Enter the path to the CSV data file: ")
+#     analysis_results = analyze_data(data_file)
+#     print(analysis_results)
+
+
+import streamlit as st
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
-def analyze_data(file_path):
-    data = pd.read_excel("topic_detect_final_test.xlsx")
+def main():
+    st.title("Machine Learning Application")
     
-    # Perform analysis on the data (you can customize this part)
-    summary = data.describe()
+    uploaded_file = st.file_uploader("Upload a CSV file", type=["csv"])
     
-    return summary
-
-if __name__ == "__main__":
-    data_file = input("Enter the path to the CSV data file: ")
-    analysis_results = analyze_data(data_file)
-    print(analysis_results)
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        st.write("Uploaded Data:")
+        st.write(data)
+        
+        X = data.drop('target_column', axis=1)  # Replace 'target_column' with your target column name
+        y = data['target_column']
+        
+        model = LinearRegression()
+        model.fit(X, y)
+        
+        st.write("Model Coefficients:", model.coef_)
+        st.write("Model Intercept:", model.intercept_)
+        
+if _name_ == "_main_":
+    main()
