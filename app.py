f1999bdd8572d82468757c557befc82f7620fad3
Sudharshan Karthik <globusitsudharshan@gmail.com>
2023-08-11 03:57:34 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def analyze_data(file_path):
+    data = pd.read_csv(file_path)
+    
+    # Perform analysis on the data (you can customize this part)
+    summary = data.describe()
+    
+    return summary
+
+if __name__ == "__main__":
+    data_file = input("Enter the path to the CSV data file: ")
+    analysis_results = analyze_data(data_file)
+    print(analysis_results)
